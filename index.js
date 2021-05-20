ff06482b2328668c37d026098be9de892ed4f50a
Harsh <harshvardhansingh7525@gmail.com>
2021-05-20 10:37:21 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const dropZone=document.querySelector(".drop-zone");
 const browsebtn=document.querySelector(".browsebtn");
-const fileinput = document.querySelector("#fileinput")
+const fileinput = document.querySelector("#fileinput");
+
+const progressContainer=document.querySelector(".progress-container");
+const bgProgress =document.querySelector(".bg-Progress");
+const percentDiv =document.querySelector("#percent");
+
 const host =" https://innshare.herokuapp.com/"  //it will change after backend
 const uploadURL = host + "api/files"; //gateway
 //const uploadURL = host + "api/files";  //for email, vid 56.15min
@@ -42,19 +47,38 @@ dropZone.addEventListener("drop", (e)=>{
 
  ////////////////////////////////////////////////upload function/////////
  const uploadFile = ()=>{
+     progressContainer.style.display="block";
      const file =fileinput.files[0]
      const formData = new FormData()
      formData.append("myfile", file);  //some backend concept
 
      const xhr= new XMLHttpRequest();
-     xhr.onreadystatechange= ()=>{
+
+     xhr.onreadystatechange= ()=>{  //when event khatam hua
         // console.log(xhr.readyState);
 
         if(xhr.readyState=== XMLHttpRequest.DONE){
             console.log(xhr.response);
+            showLink(JSON.parse(xhr.response)) //parse lagane se javascript object bngya 
         }
      };
 
+   xhr.upload.onprogress= updateProgress;//progress of upload batayega///////////////////////////  
+
+
+
      xhr.open("POST", uploadURL);
      xhr.send(formData)
+ };
+
+ const updateProgress=(e)=>{ /////progress fnt
+   //  console.log(e);
+     const percent= Math.round((e.loaded/e.total) *100);
+     bgProgress.style.width = `${percent}%`
+     percentDiv.innerText =percent;
+ };
+
+ const showLink=({file})=>{
+       console.log(file);
+       progressContainer.style.display="none"; //1:36 hr
  }
